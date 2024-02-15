8047d3122ca48ac0f7c74dd0ce76c731e8f90092
Dan Mihai <dmihai@microsoft.com>
2024-02-15 22:06:31 +0000

    Hack build.rs

--- build.rs
+++ build.rs
@@ -6,6 +6,7 @@ use std::path::Path;
 use std::process::Command;
 
 fn main() -> Result<()> {
+/*
     // Copy hooks to appropriate location so that git will run them.
     // In git worktrees, .git is a symlink and the following commands fail.
     if Path::new(".git").is_dir() {
@@ -20,6 +21,7 @@ fn main() -> Result<()> {
         .unwrap();
     let git_hash = String::from_utf8(output.stdout).unwrap();
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+*/
 
     Ok(())
 }
